3da799c4237b46408ae8b7d09a0afdb4222cade5
Mark <markyb152@hotmail.com>
2023-07-08 17:37:10 +1000

    navbar style

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import { NavBar } from "../components/componentsindex";
 import "../styles/globals.css";
+
+//INTERNAL IMPORT
+
 
 const MyApp = ({ Component, pageProps }) => (
   <div>
+    <NavBar />
     <Component {...pageProps} />
   </div>
 );
-
 export default MyApp;
